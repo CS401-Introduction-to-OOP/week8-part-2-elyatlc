be2ada0dc22cf7f6ca4b6a33a077dab27670b5b1
elina <tolochina.elya@gmail.com>
2026-03-28 18:13:51 +0200

    fixed task 2

--- Week8/BoxingTester.cs
+++ Week8/BoxingTester.cs
@@ -4,11 +4,12 @@ public static class BoxingTester
 {
     public static void Test()
     {
-        int a = 10;
-        object b = a;
-        int c = (int)b;
+        int a = 10; // значення зберігається в стек
+        object b = a; // створюється посилання на значення в купу
+        int c = (int)b; // система переходить по посиланню з купи, копіює значення та додає в стек для змінної 'с'
         Console.WriteLine($"The original value is: {a}");
         Console.WriteLine($"The boxed value is: {b}");
         Console.WriteLine($"The unboxed value is: {c}");
+        
     }
 }
